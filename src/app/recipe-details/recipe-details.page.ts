d36bac9cd48f5c800a08b89266cf831d0df5a041
SaraMitchell1 <G00438824@atu.ie>
2025-12-16 19:50:36 +0000

    Updated to include favourites button with local storage. Still need to fix rotuing issue to favourites/settings page

--- src/app/recipe-details/recipe-details.page.ts
+++ src/app/recipe-details/recipe-details.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; 
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -7,6 +7,7 @@ import {
   IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, 
   IonLabel, IonButton, IonThumbnail 
 } from '@ionic/angular/standalone';
+import { Recipe } from '../models/recipe';
 
 @Component({
   standalone: true,
@@ -30,38 +31,63 @@ import {
 export class RecipeDetailsPage implements OnInit {
   recipeId: number = 0;
   measurementUnit: 'metric' | 'us' = 'metric';
-  recipe: any = {};  
+  recipe: any = {};
+  isFavourite: boolean = false;
 
- 
   constructor(private route: ActivatedRoute, private router: Router, private http: HttpClient) {}
 
   ngOnInit() {
-    
     const id = this.route.snapshot.paramMap.get('id');
     if (id) this.recipeId = +id;
 
-   
     const savedUnit = localStorage.getItem('measurementUnit');
     if (savedUnit === 'metric' || savedUnit === 'us') this.measurementUnit = savedUnit;
 
-    
     this.fetchRecipeDetails(this.recipeId);
+    this.loadFavouriteStatus();
   }
 
   fetchRecipeDetails(id: number) {
     const apiKey = '70759a4f7911402abcc53d3c51d3b759'; 
     const url = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}`;
-
-   
     this.http.get<any>(url).subscribe(
       (res) => {
-        console.log('Recipe details fetched:', res);
         this.recipe = res; 
       },
       (err) => {
         console.error('Error fetching recipe details:', err);
       }
     );
+  }
+
+  loadFavouriteStatus() {
+    const favouritesJSON = localStorage.getItem('favourites');
+    if (favouritesJSON) {
+      const favourites: Recipe[] = JSON.parse(favouritesJSON);
+      this.isFavourite = favourites.some(r => r.id === this.recipeId);
+    }
+  }
+
+  toggleFavourite() {
+    let favourites: Recipe[] = [];
+    const favouritesJSON = localStorage.getItem('favourites');
+    if (favouritesJSON) {
+      favourites = JSON.parse(favouritesJSON);
+    }
+
+    if (this.isFavourite) {
+      favourites = favourites.filter(r => r.id !== this.recipeId);
+      this.isFavourite = false;
+    } else {
+      favourites.push({
+        id: this.recipe.id,
+        title: this.recipe.title,
+        image: this.recipe.image
+      });
+      this.isFavourite = true;
+    }
+
+    localStorage.setItem('favourites', JSON.stringify(favourites));
   }
 
   goBack() {
